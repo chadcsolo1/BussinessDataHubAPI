93ec4153f6074aa1fd8006ce4848ece630a9c82f
chad solomon <chadcsoloman@gmail.com>
2025-04-02 01:58:46 -0400

    Add project files.

---
+++ API_GraphQL/Data/Query.cs
@@ -0,0 +1,25 @@
+﻿using API_GraphQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_GraphQL.Data
+{
+    public class Query
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public async Task<List<SalesPerson>> GetSalesPeople([Service(ServiceKind.Synchronized)] AdventureWorks2017Context _context)
+        {
+            return await _context.SalesPeople.Include(x => x.BusinessEntity).Include(x => x.SalesOrderHeaders).ToListAsync();
+        }
+
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public async Task<SalesPerson> GetSalesPerson([Service(ServiceKind.Synchronized)] AdventureWorks2017Context _context, int Id)
+        {
+            return await _context.SalesPeople.Include(x => x.SalesPersonQuotaHistories).Include(x => x.SalesOrderHeaders)
+                        .Where(x => x.BusinessEntityId == Id).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ API_GraphQL/Program.cs
@@ -0,0 +1,53 @@
+using API_GraphQL.Data;
+using API_GraphQL.Models;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AdventureWorks2017Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: "CorsPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddGraphQLServer()
+    .RegisterDbContext<AdventureWorks2017Context>(DbContextKind.Synchronized)
+    .AddQueryType<Query>()
+    //.AddMutationType<Mutation>()
+    .AddProjections().AddFiltering().AddSorting();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("CorsPolicy");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGraphQL("/graphql");
+
+app.Run();
