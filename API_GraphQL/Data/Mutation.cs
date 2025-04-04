28d58a83767c6acf711ec75df0787f5974898433
chad solomon <chadcsoloman@gmail.com>
2025-04-03 21:42:36 -0400

    Added and Tested Create, Update, and Delete functionality.

--- API_GraphQL/Data/Mutation.cs
+++ API_GraphQL/Data/Mutation.cs
@@ -1,29 +1,55 @@
 ﻿using API_GraphQL.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_GraphQL.Data
 {
     public class Mutation
     {
-        public async Task<SalesPerson> CreateSalesPerson([Service(ServiceKind.Synchronized)] AdventureWorks2017Context _context,
-            int territoryId, decimal salesQuota, decimal bonus, decimal commissionPct, decimal salesYTD, decimal salesLastYear,
-            Guid rowGuid, DateTime modifiedDate)
+        public async Task<Employee> CreateEmployee([Service(ServiceKind.Synchronized)] AdventureWorks2017Context _context, string nationalIdNumber, string loginID,
+            short organizationLevel, string jobTitle, DateOnly birthDate, string maritalStatus, string gender, DateOnly hireDate, 
+            int salariedFlag, short vacationHours, short sickLeaveHours, int currentFlag)
         {
-            SalesPerson newSalesPerson = new SalesPerson
+            //int businessEntityId, 
+            //SalesPerson newSalesPerson = new SalesPerson
+            //{
+            //    //BusinessEntityId = businessEntityId,
+            //    TerritoryId = territoryId,
+            //    SalesQuota = salesQuota,
+            //    Bonus = bonus,
+            //    CommissionPct = commissionPct,
+            //    SalesYtd = salesYTD,
+            //    SalesLastYear = salesLastYear,
+            //    Rowguid = new Guid(),
+            //    ModifiedDate = DateTime.UtcNow
+            //};
+            var businessEntityIdList = await _context.Employees.ToListAsync();
+            var bussinessEntityId = businessEntityIdList.OrderByDescending(x => x.BusinessEntityId).FirstOrDefault();
+
+            Employee newEmployee = new Employee
             {
-                TerritoryId = territoryId,
-                SalesQuota = salesQuota,
-                Bonus = bonus,
-                CommissionPct = commissionPct,
-                SalesYtd = salesYTD,
-                SalesLastYear = salesLastYear,
-                Rowguid = rowGuid,
+                BusinessEntityId = bussinessEntityId.BusinessEntityId + 1,
+                NationalIdnumber = nationalIdNumber,
+                LoginId = loginID,
+                OrganizationLevel = organizationLevel,
+                JobTitle = jobTitle,
+                BirthDate = birthDate,
+                MaritalStatus = maritalStatus,
+                Gender = gender,
+                HireDate = hireDate,
+                SalariedFlag = salariedFlag == 1,
+                VacationHours = vacationHours,
+                SickLeaveHours = sickLeaveHours,
+                CurrentFlag = currentFlag == 1,
+                Rowguid = Guid.NewGuid(),
                 ModifiedDate = DateTime.UtcNow
+
             };
 
-            _context.SalesPeople.Add(newSalesPerson);
+            _context.Employees.Add(newEmployee);
             await _context.SaveChangesAsync();
-            return newSalesPerson;
+            return newEmployee;
         }
 
         //public async Task<SalesPerson> CreateSalesPerson([Service(ServiceKind.Synchronized)] AdventureWorks2017Context _context,
@@ -45,5 +71,48 @@ namespace API_GraphQL.Data
         //    await _context.SaveChangesAsync();
         //    return newSalesPerson;
         //}
+
+        public async Task<SalesPerson> UpdateSalesPerson([Service(ServiceKind.Synchronized)] AdventureWorks2017Context _context,
+            decimal bonus, int businessEntityId)
+        {
+            var salesPerson = await _context.SalesPeople.Where(x => x.BusinessEntityId == businessEntityId).FirstOrDefaultAsync();
+
+            if (salesPerson != null)
+            {
+                salesPerson.Bonus = bonus;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return salesPerson;
+        }
+
+        public async Task<DeleteEntityResponse> DeleteSalesPerson([Service(ServiceKind.Synchronized)] AdventureWorks2017Context _context, int businessEntityId)
+        {
+            DeleteEntityResponse deleteEntityResponse = new();
+            var salesPerson = await _context.SalesPeople.FirstOrDefaultAsync(x => x.BusinessEntityId == businessEntityId);
+
+            if (salesPerson == null)
+            {
+                deleteEntityResponse.Status = false;
+                deleteEntityResponse.Message = "The targeted entity was not deleted.";
+                return deleteEntityResponse;
+            }
+
+            _context.SalesPeople.Remove(salesPerson);
+            await _context.SaveChangesAsync();
+
+            deleteEntityResponse.Status = true;
+            deleteEntityResponse.Message = $"The intended entity {businessEntityId} with a type of was deleted successfully";
+
+            return deleteEntityResponse;
+
+        }
+
+
+
+
+
+
     }
 }
---
+++ API_GraphQL/Models/DeleteEntityResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace API_GraphQL.Models
+{
+    public class DeleteEntityResponse
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
